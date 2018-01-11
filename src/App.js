3c5e4064a5f3085f27979292b95e8e1e28d0200e
David Aguilar <davidaguilarh.94@gmail.com>
2018-01-11 00:33:29 -0500

    Fix participants list functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.updateName = this.updateName.bind(this)
+    this.updateLastName = this.updateLastName.bind(this)
+    this.addName = this.addName.bind(this)
+    this.state = {
+      list:[],
+      name: '',
+      lastName: ''
+    }
+  }
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.addName}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" className="form-control" name="first-name" value={this.state.name} onChange={this.updateName}/>
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" className="form-control" name="last-name"  value={this.state.lastName} onChange={this.updateLastName}/>
               </div>
 
               <div className="action">
@@ -30,7 +41,12 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+              {this.state.list.map((person, index) =>
+                <tr key={index}>
+                <td>{person.name}</td>
+                <td>{person.lastName}</td>
+                </tr>
+              )}
               </tbody>
             </table>
           </div>
@@ -38,8 +54,26 @@ class App extends Component {
       </div>
     )
   }
+
+  updateName(e) {
+    this.setState({
+      name: e.target.value
+    })
+  }
+  updateLastName(e) {
+    this.setState({
+      lastName: e.target.value
+    })
+  }
+
+  addName(e){
+    e.preventDefault()
+    this.setState({
+      list: this.state.list.concat({name: this.state.name, lastName: this.state.lastName}),
+      name: '',
+      lastName: ''
+    })
+  }
 }
 
 export default App
-
-
